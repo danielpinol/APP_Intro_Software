25874625c09979006641df579853a32fca8f225b
Daniel Piñol <daniel.pinol@ufm.edu>
2026-04-25 01:17:13 -0600

    Setup routing for inicio, solicitar-lavado, mis-pedidos and admin pages

---
+++ Proyecto_final/frontend/src/app/Componentes/solicitar-lavado/solicitar-lavado.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitar-lavado',
+  imports: [],
+  templateUrl: './solicitar-lavado.html',
+  styleUrl: './solicitar-lavado.css',
+})
+export class SolicitarLavado {}
--- Proyecto_final/frontend/src/app/app.routes.ts
+++ Proyecto_final/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './Componentes/inicio/inicio';
+import { SolicitarLavado } from './Componentes/solicitar-lavado/solicitar-lavado';
+import { MisPedidos } from './Componentes/mis-pedidos/mis-pedidos';
+import { Admin } from './Componentes/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+  { path: 'solicitar', component: SolicitarLavado },
+  { path: 'mi-orden', component: MisPedidos },
+  { path: 'admin', component: Admin },
+  { path: '**', redirectTo: '' }
+];
