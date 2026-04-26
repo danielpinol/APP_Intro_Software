dbe917be0724f33f1de408dde164e49d7f0d89f3
Alexander L. <alexavion1028@gmail.com>
2026-04-26 10:47:56 -0600

    login html

--- Proyecto_final/frontend/src/app/Componentes/log-in/log-in.ts
+++ Proyecto_final/frontend/src/app/Componentes/log-in/log-in.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-log-in',
+  standalone: true,
   imports: [],
   templateUrl: './log-in.html',
-  styleUrl: './log-in.css',
+  styleUrl: './log-in.css'
 })
 export class LogIn {}
