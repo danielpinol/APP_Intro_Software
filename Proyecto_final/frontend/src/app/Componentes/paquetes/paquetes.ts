2a4138560095a0895add3e1d36803d4be3480997
Alexander L. <alexavion1028@gmail.com>
2026-04-26 10:29:02 -0600

    Component packages created, route added into app routes

---
+++ Proyecto_final/frontend/src/app/Componentes/paquetes/paquetes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paquetes',
+  imports: [],
+  templateUrl: './paquetes.html',
+  styleUrl: './paquetes.css',
+})
+export class Paquetes {}
