a36db164edf84845692e9876e6a51b62e2c925d8
Daniel Piñol <daniel.pinol@ufm.edu>
2026-04-26 13:05:19 -0600

    Routerlink import fixed in solicitarlavados and paquetes

---
+++ Proyecto_final/frontend/src/app/Componentes/paquetes/paquetes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-paquetes',
+  imports: [RouterLink],
+  templateUrl: './paquetes.html',
+  styleUrl: './paquetes.css',
+})
+export class Paquetes {}
--- Proyecto_final/frontend/src/app/Componentes/solicitar-lavado/solicitar-lavado.ts
+++ Proyecto_final/frontend/src/app/Componentes/solicitar-lavado/solicitar-lavado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-solicitar-lavado',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './solicitar-lavado.html',
   styleUrl: './solicitar-lavado.css',
 })
