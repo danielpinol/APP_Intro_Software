1b1b335b0fa2a5230b3afb7e2c7ad1cfc2dbb907
Alexander L. <alexavion1028@gmail.com>
2026-04-29 09:24:46 -0600

    Test of get data from pedidos.js

--- Proyecto_final/backend/server_client.js
+++ Proyecto_final/backend/server_client.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 
@@ -7,14 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('Backend funcionando !!!!!!!!!!');
+// RUTA
+app.get('/api/pedidos', (req, res) => {
+  try {
+    const data = fs.readFileSync('pedidos.json', 'utf-8');
+    const pedidos = JSON.parse(data);
+
+    res.send(JSON.stringify(pedidos, null, 2));
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error leyendo pedidos' });
+  }
 });
 
 // Puerto
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}/api/pedidos`);
 });
